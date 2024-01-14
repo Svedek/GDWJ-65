b779efe6bc5e61d145285d8ddc4f9a61952c45dc
Svedek <28320440+Svedek@users.noreply.github.com>
2024-01-14 17:54:50 -0600

    inputs added

--- GDWJ-65/Actors/Player/player.gd
+++ GDWJ-65/Actors/Player/player.gd
@@ -7,7 +7,7 @@ const JUMP_VELOCITY = -200.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
+# move_left, move_right, jump, fall, drop_inventory
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -17,6 +17,12 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+	
+
+	move_and_slide()
+
+
+func _handle_walk():
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
@@ -24,5 +30,5 @@ func _physics_process(delta):
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+		
+		
